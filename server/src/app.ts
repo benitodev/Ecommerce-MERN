f80f3973e085e2911df3ae55d8a8ac34a1bc6ca3
benitodev <benideveloperr@gmail.com>
2023-02-27 17:11:22 -0300

    add JWT_SECREFT_REFRESH config property

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,20 @@
-import express, { json } from 'express';
+import express, { Application, json } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import routes from './utils/routes.utils';
 import db from './database/connect.db';
-
+import { version } from '../package.json';
 const PORT = process.env.PORT || 3001;
-const app = express();
 
+const routeContainer = (app: Application) => () => routes(app);
+
+const app: Application = express();
+
+const versionApi = version.charAt(0);
 app.use(cors());
 app.use(json());
+
+// app.use(`/v${versionApi}`, routeContainer(app));
 routes(app);
 db()
   .then(() => console.log('db connected'))
--- server/src/config.ts
+++ server/src/config.ts
@@ -4,8 +4,9 @@ const config = {
   //db config
   DB_URI: process.env.DB_URI ?? 'mongodb://0.0.0.0:27017/',
   DB_NAME: process.env.DB_NAME ?? 'eCommerce',
-
+  DB_NAME_TEST: process.env.DB_NAME_TEST || 'e_commerce_test',
   JWT_SECRET: process.env.JWT_SECRET ?? 'secret-token',
+  JWT_SECRET_REFRESH: process.env.JWT_SECRET_REFRESH ?? 'secret-token-refresh',
 };
 
 export default config;
