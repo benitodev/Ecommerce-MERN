b75da2e9ec6a7cc17779a829ae90a332f52278f8
benitodev <benideveloperr@gmail.com>
2023-02-27 16:59:50 -0300

    update cypress.config to use a baseUrl

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
+import { PluginOption, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as PluginOption],
   server: {
     watch: {
       usePolling: true,
