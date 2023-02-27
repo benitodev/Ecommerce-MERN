480c9a9e9b74d941ee3679529e06ae79f294b785
benitodev <benideveloperr@gmail.com>
2023-02-27 17:06:48 -0300

    add route refreshtoken

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as userRoutes } from './user/user.routes';
 export { default as productRoutes } from './product/product.routes';
 export { default as cartRoutes } from './cart/cart.routes';
+export { default as testRoutes } from './test/test.routes';
--- server/src/routes/user/user.routes.ts
+++ server/src/routes/user/user.routes.ts
@@ -3,6 +3,7 @@ import * as controllers from '../../controllers';
 import { USER_ENDPOINTS } from '../../controllers';
 import { validatorSchema } from '../../middlewares/validatorSchema.middleware';
 import { registerUserSchema } from '../../schemas';
+import { tokenAuthorization } from '../../middlewares/authorization';
 
 const userRoutes: IRouter = Router();
 
@@ -21,4 +22,7 @@ userRoutes.put(USER_ENDPOINTS.UPDATE_USER, controllers.updateUser);
 
 userRoutes.delete(USER_ENDPOINTS.DELETE, controllers.deleteUser);
 
+userRoutes.post(USER_ENDPOINTS.LOGOUT, tokenAuthorization, controllers.logout);
+
+userRoutes.post(USER_ENDPOINTS.REFRESH_TOKEN, controllers.refreshToken);
 export default userRoutes;
