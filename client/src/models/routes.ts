3972bf8e9caec8fa418083a94d4ff8662f00f447
benitodev <benideveloperr@gmail.com>
2023-02-27 16:39:29 -0300

    add private routes

--- client/src/models/routes.ts
+++ client/src/models/routes.ts
@@ -2,4 +2,10 @@ export enum PUBLIC_ROUTES {
   LOGIN = 'login',
   REGISTER = 'register',
   SHOP = 'shop',
+  SINGLE_PRODUCT = 'product/:id',
+}
+
+export enum PRIVATE_ROUTES {
+  BASKET = 'basket',
+  WISHLIST = 'wishlist',
 }
