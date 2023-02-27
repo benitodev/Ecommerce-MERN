68bb22299370cf66f8d8bcf32bae28bf8c32aa43
benitodev <benideveloperr@gmail.com>
2023-02-27 17:10:04 -0300

    add testing route if the environment 'test' exists

--- server/src/utils/routes.utils.ts
+++ server/src/utils/routes.utils.ts
@@ -1,10 +1,13 @@
 import { Application } from 'express';
-import { cartRoutes, productRoutes, userRoutes } from '../routes';
+import { cartRoutes, productRoutes, testRoutes, userRoutes } from '../routes';
 
 const routes = (server: Application) => {
   server.use('/user', userRoutes);
   server.use('/product', productRoutes);
   server.use('/cart', cartRoutes);
+  if (process.env.NODE_ENV === 'test') {
+    server.use('/testing', testRoutes);
+  }
 };
 
 export default routes;
