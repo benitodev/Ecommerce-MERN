de541be99bca812fc3bae564c93fdfc0f5f20fc8
benitodev <benideveloperr@gmail.com>
2023-02-27 16:35:16 -0300

    update component to use optionsContext

--- client/src/components/forms/FormInputSelect.tsx
+++ client/src/components/forms/FormInputSelect.tsx
@@ -2,37 +2,33 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Dispatch, SetStateAction, useContext, useState } from 'react';
-import { OptionActions } from '../../reducers/OptionsReducer';
+import {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { OptionsContext } from '../../context/OptionsContext';
 
 interface Props {
+  id?: string;
   label: string;
   elements: Array<string> | [];
   styles?: {};
-  optionKey?: string;
-  distpatch?: React.Dispatch<OptionActions>;
 }
 
-const FormInputSelect = ({
-  label,
-  styles,
-  elements,
-  optionKey,
-  distpatch,
-}: Props) => {
-  const [selectValue, setSelectValue] = useState<string>('');
-
+const FormInputSelect = ({ id, label, styles, elements }: Props) => {
+  const [selectValue, setSelectValue] = useState<string>(elements[0]);
+  const labelProperty = label.toLowerCase();
+  const { setOptions } = useContext(OptionsContext);
   const handleChange = (event: SelectChangeEvent) => {
     const { value } = event.target;
-    if (optionKey && value && distpatch) {
-      distpatch({
-        type: 'add',
-        payload: { key: optionKey, value },
-      });
-    } else {
-      setSelectValue(value);
-    }
+    setSelectValue(value);
   };
+  useEffect(() => {
+    setOptions((items) => {
+      return { ...items, [labelProperty]: selectValue };
+    });
+  }, [selectValue]);
   return (
     <FormControl sx={{ width: '100%', maxWidth: '300px' }}>
       <InputLabel sx={{ textAlign: 'center' }}>{label}</InputLabel>
